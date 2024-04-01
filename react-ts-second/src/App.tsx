3782fb5acd08644298073808b5a27c928c2413e3
Mario <jorge.fernandez@pragma.com.co>
2024-03-31 20:36:42 -0500

    Add input component

---
+++ react-ts-second/src/App.tsx
@@ -0,0 +1,12 @@
+import Input from "./components/input";
+
+function App() {
+  return (
+    <main>
+      <Input id="name" label="Your name"></Input>
+      <Input id="age" label="Your age"></Input>
+    </main>
+  );
+}
+
+export default App;
